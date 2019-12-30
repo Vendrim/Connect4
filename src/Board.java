b86b5e8f8f40ee751ac5399fd4a75463562fb620
vbialek <Victor.Bialek@hs-augsburg.de>
2019-12-30 14:59:55 +0000

    Board Class finalized

--- src/Board.java
+++ src/Board.java
@@ -1,28 +1,40 @@
-public class board {
-    static String[][] board = new String[10][10];
+public class Board {
+    private String[][] board;
 
-    public static String[][] Fill_up_Board(String[][] to_fill) {
-        String[][] filled_board = new String[to_fill.length][to_fill[0].length];
+    public Board(int x, int y) {
+        board = Fill_up_Board(x, y);
+    }
 
+    public void init_board() {
+        board[3][4] = "X";
+        printTopLine();
+        printColumns();
+        printNumberPart();
+    }
+
+    public String[][] Fill_up_Board(int x, int y) {
+        String[][] filled_board = new String[x][y];
         for (int i = 0; i < filled_board.length; i++) {
             for (int j = 0; j < filled_board[i].length; j++) {
                 filled_board[i][j] = " ";
             }
         }
-
-
         return filled_board;
     }
 
-    public static void init_board() {
+    public void insertIntoBoard(String character, int pos_X, int pos_Y) {
+        board[pos_X][pos_Y] = character;
+    }
 
+    public void insertIntoBoard(char character, int pos_X, int pos_Y) {
+        board[pos_X][pos_Y] = Character.toString(character);
+    }
 
-        board =
+    public String getFromBoard(int pos_X, int pos_Y){
+        return board[pos_X][pos_Y];
+    }
 
-                Fill_up_Board(board);
-
-        board[3][4] = "X";
-
+    private void printTopLine() {
         for (
                 int j = 0;
                 j < board.length; j++) {
@@ -36,6 +48,9 @@ public class board {
                 System.out.print("___");
             }
         }
+    }
+
+    private void printColumns() {
         for (
                 int i = 0;
                 i < board.length; i++) {
@@ -64,9 +79,9 @@ public class board {
 
 
         }
-        for (
-                int i = 0;
-                i < board.length; i++) {
+    }
+    private void printNumberPart(){
+        for (int i = 0; i < board.length; i++) {
             if (i <= 10) {
                 System.out.print(" |");
             } else {
@@ -75,6 +90,8 @@ public class board {
             System.out.print(i);
         }
         System.out.println(" |");
-
     }
+
+
+
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        Board board = new Board(10,10);
+        board.init_board();
     }
 
 }
